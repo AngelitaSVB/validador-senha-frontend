dd4a1f0266d0c88a0e8ca92b8378784ff3e41d6f
Angelita Vilas Boas <angelitasvb@gmail.com>
2025-07-31 12:29:32 -0300

    Altaração no front

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
-import { environment } from '../../environments/environment.dev';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -12,10 +12,12 @@ export class AuthService {
   getToken(): Observable<string> {
     const body = new HttpParams()
       .set('grant_type', 'client_credentials')
-      .set('client_id', 'frontend-itau')
-      .set('client_secret', 'segredo123');
+      .set('client_id', 'frontend-itau') // você pode tornar isso seguro depois no backend
+      .set('client_secret', 'segredo123'); // idealmente não deveria estar no frontend
 
-    return this.http.post<any>(`${environment.apiUrl}/oauth/token`, body).pipe(
+    return this.http.post<any>(`${environment.apiUrl}/oauth/token`, body, {
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+    }).pipe(
       map(response => response.access_token)
     );
   }
